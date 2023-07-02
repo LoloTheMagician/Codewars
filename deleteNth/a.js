2cfed8036174d3db22df6a840f4e027641188414
Lolo <elperiplodelolo@gmail.com>
2023-07-02 19:23:25 +0200

    new changes

---
+++ deleteNth/a.js
@@ -0,0 +1,6 @@
+const deleteNth = (numbers, n) => {
+    
+
+}
+
+console.log(deleteNth([20,37,20,21], 1))
---
+++ simplePigLatin/a.js
@@ -0,0 +1,7 @@
+const changeString = (string) => {
+    return string.split(' ').map(pal => {
+        
+    }).join(' ')
+}
+
+console.log(changeString('Pig latin is cool'))
